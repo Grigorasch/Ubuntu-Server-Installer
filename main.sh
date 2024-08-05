13fa33b6b5517e50c54879d717b30eb994b8279c
Григорий <122614065+Grigorasch@users.noreply.github.com>
2024-08-05 16:54:18 +0600

    Update main.sh
    Добавлено обновление пакетов
    Добавлено меню выбора действия
    Добавлена установка git и curl

--- main.sh
+++ main.sh
@@ -1,30 +1,39 @@
-#!/bin/bash
+# Обновление пакетов
+read -p "Выполнить обновление пакетов? ([y]/n): " update_choice
+if [[ -z "$update_choice" ]]; then
+    update_choice="y"
+fi
+if [[ "$update_choice" == "y" ]]; then
+    echo "Обновление списка пакетов и сами пакеты."
+    apt update && apt upgrade -y
+fi
+
 
 # Функция для отображения меню
 show_menu() {
     clear
-    echo "Выберите пункт меню:"
-    echo "1) Пункт один"
-    echo "2) Пункт два"
-    echo "3) Пункт три"
-    echo "4) Выход"
-    read -p "Введите номер пункта (1-4): " choice
+    echo "Выберите дальнейшее действие:"
+    echo "1) Установить базовые программы"
+    echo "e) Выход"
+    read -p "Введите номер пункта: " choice
 }
 
 while true; do
     show_menu
-
     case $choice in
         1)
-            bash пункт_один.sh
+            echo "Будут установлены следующие программы:"
+            echo "Curl"
+            echo "Git"
+            echo "GH"
+            echo "Нажмите [Enter] для продолжения..."
+            read -p "" temp
+            
+            bash ~/newsrv/soft/curl.sh -i
+            bash ~/newsrv/soft/git.sh -i
+            bash ~/newsrv/soft/git.sh -a
             ;;
-        2)
-            bash пункт_два.sh
-            ;;
-        3)
-            bash пункт_три.sh
-            ;;
-        4)
+        e)
             echo "Выход из программы."
             break
             ;;
@@ -32,6 +41,5 @@ while true; do
             echo "Неверный выбор, пожалуйста выберите 1, 2, 3, или 4."
             ;;
     esac
-
     read -p "Нажмите [Enter], чтобы продолжить..." temp
 done
